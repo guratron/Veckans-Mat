edb9e957e5330c1e7c694aeca685d737404a07cc
guratron <46029264+guratron@users.noreply.github.com>
2018-12-20 17:45:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,15 @@
     // configuration =================
 
     
-    mongoose.connect('mongodb://gdice:<PASSWORD>@todo-shard-00-00-ekauy.mongodb.net:27017,todo-shard-00-01-ekauy.mongodb.net:27017,todo-shard-00-02-ekauy.mongodb.net:27017/test?ssl=true&replicaSet=ToDo-shard-0&authSource=admin&retryWrites=true', { useNewUrlParser: true });
+    mongoose.connect('mongodb://gdice:<PASSWORD>@todo-shard-00-00-ekauy.mongodb.net:27017,todo-shard-00-01-ekauy.mongodb.net:27017,todo-shard-00-02-ekauy.mongodb.net:27017/test?ssl=true&replicaSet=ToDo-shard-0&authSource=admin&retryWrites=true', 
+    { useNewUrlParser: true }, function(err) {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log("Server started successfully!")
+        }
+    });
 
     app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
 
